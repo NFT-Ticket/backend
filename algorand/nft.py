78cf5463bc0b3ed2f0e1a4f264d19897efbd0cf8
iamroshanpoudel <roshan.poudel@stonybrook.edu>
2022-04-19 00:02:09 -0400

    nft transfer and opt in modules added

--- algorand/nft.py
+++ algorand/nft.py
@@ -1,13 +1,14 @@
 from algosdk.future.transaction import *
-import client
-from algoaccount import *
-from utilities import *
+from .client import Client
+from .algoaccount import AlgorandAccount
+from . import utilities
+import json
 
 
 # Unit name should be 8 char or less
 
 def create_nft(nft_name, unit_name, amt, creator, manager, url):
-    algod_client = client.get_algod_client()
+    algod_client = Client.get_algod_client()
 
     # Get network params for transactions before every transaction.
     params = algod_client.suggested_params()
@@ -57,8 +58,10 @@ def create_nft(nft_name, unit_name, amt, creator, manager, url):
         # Get the new asset's information from the creator account
         ptx = algod_client.pending_transaction_info(txn_id)
         asset_id = ptx["asset-index"]
-        print_created_asset(algod_client, creator.public_key, asset_id)
-        print_asset_holding(algod_client, creator.public_key, asset_id)
+        utilities.print_created_asset(
+            algod_client, creator.public_key, asset_id)
+        utilities.print_asset_holding(
+            algod_client, creator.public_key, asset_id)
 
     except Exception as e:
         print(e)
@@ -66,8 +69,57 @@ def create_nft(nft_name, unit_name, amt, creator, manager, url):
     return asset_id
 
 
+def opt_in_to_nft(sender, receiver, asset_id):
+    '''
+    For a NFT to transfer from A to B, 
+    B must opt in to receive the NFT
+    This is done via a opt_in transaction, which
+    results in the asset_id with balance 0 in B's account
+    '''
+    algod_client = Client.get_algod_client()
+    # OPT-IN
+    # Check if asset_id is in receivers's asset holdings prior
+    # to opt-in
+    params = algod_client.suggested_params()
+    account_info = algod_client.account_info(receiver.public_key)
+    holding = None
+    idx = 0
+    for my_account_info in account_info['assets']:
+        scrutinized_asset = account_info['assets'][idx]
+        idx = idx + 1
+        if (scrutinized_asset['asset-id'] == asset_id):
+            holding = True
+            break
+    if not holding:
+        # Use the AssetTransferTxn class to transfer assets and opt-in
+        txn = AssetTransferTxn(
+            sender=receiver.public_key,
+            sp=params,
+            receiver=receiver.public_key,
+            amt=0,
+            index=asset_id)
+        stxn = txn.sign(receiver.secret_key)
+        # Send the transaction to the network and retrieve the txid.
+        try:
+            txid = algod_client.send_transaction(stxn)
+            print("Signed transaction with txID: {}".format(txid))
+            # Wait for the transaction to be confirmed
+            confirmed_txn = wait_for_confirmation(algod_client, txid, 4)
+            print("TXID: ", txid)
+            print("Result confirmed in round: {}".format(
+                confirmed_txn['confirmed-round']))
+        except Exception as err:
+            print(err)
+        # Now check the asset holding for that account.
+        # This should now show a holding with a balance of 0.
+        utilities.print_asset_holding(
+            algod_client, receiver.public_key, asset_id)
+
+
 def transfer_nft(sender, receiver, asset_id):
-    algod_client = client.get_algod_client()
+    # Receiver should OPT in to receive NFT, if not done so already
+    opt_in_to_nft(sender, receiver, asset_id)
+    algod_client = Client.get_algod_client()
     # TRANSFER ASSET
     params = algod_client.suggested_params()
     # comment these two lines if you want to use suggested params
@@ -77,9 +129,9 @@ def transfer_nft(sender, receiver, asset_id):
         sender=sender.public_key,
         sp=params,
         receiver=receiver.public_key,
-        amt=10,
+        amt=1,
         index=asset_id)
-    stxn = txn.sign(sender.private_key)
+    stxn = txn.sign(sender. secret_key)
     # Send the transaction to the network and retrieve the txid.
     try:
         txid = algod_client.send_transaction(stxn)
@@ -92,7 +144,7 @@ def transfer_nft(sender, receiver, asset_id):
     except Exception as err:
         print(err)
     # The balance should now be 10.
-    print_asset_holding(algod_client, receiver.public_key, asset_id)
+    utilities.print_asset_holding(algod_client, receiver.public_key, asset_id)
     return txid
 
 # nft_name = "Roshan"
@@ -102,3 +154,21 @@ def transfer_nft(sender, receiver, asset_id):
 # creator = AlgorandAccount(pk, sk)
 # create_nft(nft_name, amt, creator, creator)
 # asset_id = "84222697"
+
+
+# r_pk = "V7WASWYSD7AEVPM6H46SWZB25CR7LQ5C275RE2VBCHSC7NU7FRRBKKHJK4"
+# r_sk = "K9Cd233ad3xb6rF9Z82vjluJvmujulFAeA9m8D0UPQSv7AlbEh/ASr2ePz0rZDroo/XDotf7EmqhEeQvtp8sYg=="
+# roshan = AlgorandAccount(r_sk)
+# a_pk = "G75SW7GOF3RXEOIERPRX3KBSZ4KC47VPJFVOYZHB5AOKJWV3SPWEE7F7YA"
+# a_sk = "XXeEorfCRbDNVsa7g23CEjSmwoN3NFlAO+6xxtSzrvI3+yt8zi7jcjkEi+N9qDLPFC5+r0lq7GTh6Byk2ruT7A=="
+# adrian = AlgorandAccount(a_sk)
+# asset_id = "84222697"
+# transfer_nft(roshan, adrian, asset_id)
+'''
+Signed transaction with txID: 72HSY4UYZSUMXF4DVCG73MVUMPBCZLZRRNPC467QEOWTNHFATGJA
+TXID:  72HSY4UYZSUMXF4DVCG73MVUMPBCZLZRRNPC467QEOWTNHFATGJA
+Result confirmed in round: 21064254
+Signed transaction with txID: RLOBSZ3DPGEIHL5CWUCZJTIKDLDXP2DWQNC7PPYNX5GDOCKN7X5Q
+TXID:  RLOBSZ3DPGEIHL5CWUCZJTIKDLDXP2DWQNC7PPYNX5GDOCKN7X5Q
+Result confirmed in round: 21064256
+'''
