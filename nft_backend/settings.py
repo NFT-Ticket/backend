d83b9017d6e443997febfcd60a88b4cf5c00b35d
iamroshanpoudel <roshan.poudel@stonybrook.edu>
2022-04-22 15:56:05 -0400

    static folder defined

--- nft_backend/settings.py
+++ nft_backend/settings.py
@@ -17,6 +17,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
