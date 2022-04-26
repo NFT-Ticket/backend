cfaaa24369d32bc5e25b1e61f18f3b6ca1e09911
Woo-Chester <91643598+Woo-Chester@users.noreply.github.com>
2022-04-26 00:11:51 -0400

    Update qr.py

--- qr-tiger/qr.py
+++ qr-tiger/qr.py
@@ -1,12 +1,17 @@
 import requests
 import json
+import os
+from cryptography.fernet import Fernet
+import time
+import base64
 
 
 def generate_qr(text):
 
     url = "https://qrtiger.com/api/qr/static"
-    key = "Bearer 0e7fc850-c500-11ec-a952-e7d3ec2d6654"
+    apikey = "Bearer 0e7fc850-c500-11ec-a952-e7d3ec2d6654"
 
+    encrypted_text = str(encrypt_text(text), "UTF-8")
     payload = {
                 "size": 500,
                 "colorDark": "rgb(5,64,128)",
@@ -17,15 +22,48 @@ def generate_qr(text):
                 "backgroundColor": "rgb(255,255,255)",
                 "transparentBkg": False,
                 "qrCategory": "url",
-                "text": "https://google.com?q=" + text
+                "text": "https://google.com?q=" + encrypted_text
             }
     payload = json.dumps(payload)
     headers = {
         'Content-Type': "application/json",
-        'Authorization': key
+        'Authorization': apikey
     }
     response = requests.request("POST", url, data=payload, headers=headers)
 
     res = response.json()
-    qr_url = res["url"]
+    if "url" in res:
+        qr_url = res["url"]
+    else:
+        qr_url = None
     return qr_url
+
+
+def encrypt_text(text):
+
+    # Fernet key must be 32 url-safe base 64-encoded bytes
+    key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456".encode('ascii')
+    key = base64.b64encode(key)
+    fernet = Fernet(key)
+
+    data = {"text": text,
+            "time": time.time()}
+    data = json.dumps(data)
+
+    # to encrypt the string, string must be encoded to byte string before encryption
+    encrypt_data = fernet.encrypt(data.encode())
+
+    return encrypt_data
+
+
+def decrypt_text(text):
+    
+    # Fernet key must be 32 url-safe base 64-encoded bytes
+    key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456".encode('ascii')
+    key = base64.b64encode(key)
+    fernet = Fernet(key)
+
+    text = bytes(text, "UTF-8")
+
+    decrypted_text = fernet.decrypt(text).decode()
+    return decrypted_text
